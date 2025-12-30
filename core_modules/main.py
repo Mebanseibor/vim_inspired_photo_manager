1d21ff6a875c6e82075f3795bdbcb4d0cb500048
Mebanseibor <mebanseiborlm@gmail.com>
2025-12-30 06:33:10 +0530

    Added ability to display photos
    - GUI:
      - Added image viewer
      - List of files are now displayed
        - Hard coded to the `gitignore` folder
      - Title now displays the path, instead of a generic title
    
    - File System Item:
      - Added type safety to the contructor parameter `abs_path`
      - Added the following member functions:
        - full_name()
        - is_image()
    
    - list_of_fs_items_at():
      - Added type safety to the parameters
      - Added parameter images_only to only include images
    
    - Code maintainability:
      - Centralized the displaying of the event_char

--- core_modules/main.py
+++ core_modules/main.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import tkinter as tk
 from PIL import Image, ImageTk
 
@@ -6,7 +7,7 @@ CURR_ABS_PATH = os.path.abspath("")
 
 
 class FileSystemItem:
-    def __init__(self, abs_path):
+    def __init__(self, abs_path: str):
         self.abs_path = abs_path
         self.is_file = os.path.isfile(abs_path)
 
@@ -30,9 +31,26 @@ class FileSystemItem:
         print(f"name:\t\t{self.name}")
         print(f"extension:\t{self.extension}")
 
+    def full_name(self):
+        name = self.name if self.name else ""
+        ext = f".{self.extension}" if self.extension else ""
+        return name + ext
+
+    def is_image(self) -> bool:
+        if not self.is_file:
+            return False
+
+        if not self.extension:
+            return False
+
+        if self.extension == "jpg" or self.extension == "png":
+            return True
+
+        return False
+
 
 class Result:
-    def __init__(self, result, is_successful=True, err_msg=None):
+    def __init__(self, result, is_successful: bool = True, err_msg: str | None = None):
         self.is_successful = is_successful
         self.err_msg = err_msg
         self.result = result
@@ -41,7 +59,7 @@ class Result:
         return f"Error: {self.err_msg}"
 
 
-def list_of_fs_items_at(abs_path):
+def list_of_fs_items_at(abs_path: str, images_only: bool = False):
     dir_path = os.path.join(CURR_ABS_PATH, abs_path)
 
     if not os.path.exists(dir_path):
@@ -56,7 +74,12 @@ def list_of_fs_items_at(abs_path):
     for file_path in file_paths:
         joined_path = os.path.join(dir_path, file_path)
         abs_path = os.path.abspath(joined_path)
-        files.append(FileSystemItem(abs_path))
+        fs_item = FileSystemItem(abs_path)
+        if images_only:
+            if fs_item.is_image():
+                files.append(fs_item)
+        else:
+            files.append(fs_item)
 
     return Result(files)
 
@@ -70,39 +93,83 @@ def prompt_path():
 
 
 def gui():
-    counter = 0
+    path = os.path.abspath("gitignore")
+    list = list_of_fs_items_at(path, images_only=True)
+    if not list.is_successful:
+        print(list.formatted_err_msg())
+        return
 
+    fs_items_size = len(list.result)
+    fs_item_counter = -1
+
+    # main window
     root = tk.Tk()
     root.attributes("-fullscreen", True)
     root.title("Vim Photo Manager")
-    title_l = tk.Label(root, text="This is the title")
-    file_l = tk.Label(root, text=counter)
-    mapped_key_press_label = tk.Label(root, text="Press any key")
-    command_l = tk.Label(root, text="press_label any key")
-    title_l.pack()
+
+    # components
+    currect_abs_path_l = tk.Label(root, text=path)
+    file_l = tk.Label(root, text=f"Number of images = {fs_items_size}")
+    mapped_key_press_l = tk.Label(root, text="Mapped key")
+    command_l = tk.Label(root, text="Command")
+    image_l = tk.Label(root, text="Picture place")
+
+    # packing
+    currect_abs_path_l.pack()
     file_l.pack()
-    mapped_key_press_label.pack()
+    command_l.pack()
+    mapped_key_press_l.pack()
+    image_l.pack(expand=True, fill=tk.BOTH)
+
+    if fs_items_size == 0:
+        file_l.config(text="No images here")
+
+    def event_char(char):
+        return f"'{char}'"
 
     def on_escape(event):
         root.quit()
 
     def on_left(event):
-        nonlocal counter
-        counter -= 1
-        mapped_key_press_label.config(text=event.char)
-        file_l.config(text=counter)
+        mapped_key_press_l.config(text=event_char(event.char))
+
+        nonlocal fs_item_counter
+
+        if fs_items_size == 0:
+            command_l.config(text="No image")
+            return
+
+        fs_item_counter = np.clip(fs_item_counter - 1, 0, fs_items_size - 1)
+        file_l.config(text=list.result[fs_item_counter].full_name())
         command_l.config(text="Prev photo")
+        display_image_from_path(list.result[fs_item_counter].abs_path)
 
     def on_right(event):
-        nonlocal counter
-        counter += 1
-        mapped_key_press_label.config(text=event.char)
-        file_l.config(text=counter)
+        mapped_key_press_l.config(text=event_char(event.char))
+        nonlocal fs_item_counter
+
+        if fs_items_size == 0:
+            command_l.config(text="No image")
+            return
+
+        fs_item_counter = np.clip(fs_item_counter + 1, 0, fs_items_size - 1)
+        file_l.config(text=list.result[fs_item_counter].full_name())
         command_l.config(text="Next photo")
+        display_image_from_path(list.result[fs_item_counter].abs_path)
 
     def any_key(event):
-        mapped_key_press_label.config(text=event.char)
+        mapped_key_press_l.config(text=event_char(event.char))
         command_l.config(text=event.keysym)
+
+    def display_image_from_path(path: str):
+        img = Image.open(path)
+        img.thumbnail(
+            (root.winfo_screenwidth(), root.winfo_screenheight()),
+            Image.Resampling.LANCZOS,
+        )
+        photo = ImageTk.PhotoImage(img)
+        image_l.config(image=photo)
+        image_l.image = photo
 
     root.bind("<Key>", any_key)
     root.bind("<Escape>", on_escape)
@@ -162,8 +229,10 @@ if __name__ == "__main__":
         if choice == command_quit:
             break
         elif choice == i_gui:
+            print("Opening GUI")
             gui()
         elif choice == i_cli:
+            print("Opening CLI")
             cli()
         else:
             print("Invalid choice")
